753f8f29c422a7b17b43ba493de7891473be1dfe
Jatin Chhabra <cjatin2822@gmail.com>
2023-09-07 00:49:24 +0530

    first commit

---
+++ BookkeeperAPI/Controllers/ExpenseController.cs
@@ -0,0 +1,19 @@
+﻿namespace BookkeeperAPI.Controllers
+{
+    #region usings
+    using BookkeeperAPI.Model;
+    using Microsoft.AspNetCore.Mvc;
+    #endregion
+
+    [ApiController]
+    [Route("/api/[controller]")]
+    [Produces("application/json")]
+    public class ExpenseController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Expense>> GetExpense()
+        {
+            return new List<Expense>() { new Expense() { Id = Guid.NewGuid(), Date = DateTime.Now, Name = "test expense", Category = ExpenseCategory.FinancialExpenses, Amount = 5450.00 } };
+        }
+    }
+}
---
+++ BookkeeperAPI/Program.cs
@@ -0,0 +1,51 @@
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen((options) =>
+{
+    options.DescribeAllParametersInCamelCase();
+    options.SwaggerDoc("v1", new OpenApiInfo() { Title="Bookkeeper API" });
+    options.UseInlineDefinitionsForEnums();
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
+builder.Services.AddCors((options) =>
+{
+    options.AddDefaultPolicy((policy) =>
+    {
+        policy.AllowAnyHeader();
+        policy.AllowAnyMethod();
+        policy.AllowAnyOrigin();
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    });
+}
+
+app.UseCors();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
